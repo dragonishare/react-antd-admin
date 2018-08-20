f8d62015e5a9479d84a80e500f621abffcedd570
Dragon <dragonishare@gmail.com>
2018-08-20 11:37:06 +0800

    modify sidebar and LayoutHeader

--- src/components/LayoutHeader/LayoutHeader.js
+++ src/components/LayoutHeader/LayoutHeader.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Layout, Icon, Breadcrumb } from 'antd';
+import './LayoutHeader.less';
+const { Header } = Layout;
 
-export default class LayoutHeader extends React.Component {
+class LayoutHeader extends React.Component {
   render() {
-    return <div>header</div>;
+    return (
+      <Header className="layout-header">
+        <Breadcrumb className="layout-header-left">
+          <Breadcrumb.Item>
+            <Icon type="home" />
+            <span>Home</span>
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="layout-header-right">
+          <div className="nickname">
+            <span>欢迎，admin</span>
+          </div>
+          <div className="logout">
+            <Icon type="logout" /> 注销
+          </div>
+        </div>
+      </Header>
+    );
   }
 }
+
+export default LayoutHeader;
